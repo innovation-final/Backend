264e83abb4dd59a5685dbd89e922989c806d590b
lgkrwnsdll <80141718+lgkrwnsdll@users.noreply.github.com>
2022-09-22 04:08:59 +0900

    1234

--- src/main/java/com/innovation/stockstock/controller/MemberController.java
+++ src/main/java/com/innovation/stockstock/controller/MemberController.java
@@ -2,19 +2,16 @@ package com.innovation.stockstock.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.innovation.stockstock.config.GoogleConfigUtils;
+import com.innovation.stockstock.dto.ResponseDto;
 import com.innovation.stockstock.service.GoogleMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.innovation.stockstock.service.KakaoMemberService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,52 +26,44 @@ public class MemberController {
     private final GoogleConfigUtils googleConfigUtils;
 
 
-    @GetMapping(value = "/api/member/login/google")
-    public ResponseEntity<Object> moveGoogleInitUrl() {
-        String authUrl = googleConfigUtils.googleInitUrl();
-        URI redirectUri;
-        try {
-            redirectUri = new URI(authUrl);
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.setLocation(redirectUri);
-            return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-
-        return ResponseEntity.badRequest().build();
-    }
+    //@GetMapping(value = "/api/member/login/google")
+    //public ResponseEntity<Object> moveGoogleInitUrl() {
+    //    String authUrl = googleConfigUtils.googleInitUrl();
+    //    URI redirectUri;
+    //    try {
+    //        redirectUri = new URI(authUrl);
+    //        HttpHeaders httpHeaders = new HttpHeaders();
+    //        httpHeaders.setLocation(redirectUri);
+    //        return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+    //    } catch (URISyntaxException e) {
+    //        e.printStackTrace();
+    //    }
+    //
+    //    return ResponseEntity.badRequest().build();
+    //}
 
     @GetMapping("login/oauth2/code/google")
-    public ResponseEntity<?> redirectGoogleLogin(@RequestParam(value = "code") String authCode, HttpServletResponse response) throws JsonProcessingException, URISyntaxException {
+    public ResponseEntity<?> redirectGoogleLogin(@RequestParam(value = "code") String authCode, HttpServletResponse response) throws JsonProcessingException {
         googleMemberService.googleLogin(authCode, response);
-        return goMainPageIfSuccessful();
+        return ResponseEntity.ok().body(ResponseDto.success("Google OAuth Success"));
     }
 
-    @GetMapping("/api/member/login/kakao")
-    public ResponseEntity<Object> moveKakaoInitUrl() {
-        try {
-            URI redirectUri = new URI("https://kauth.kakao.com/oauth/authorize?client_id=" + kakaoKey + "&redirect_uri=" + kakaoRedirectUrl + "&response_type=code");
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.setLocation(redirectUri);
-            return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return ResponseEntity.badRequest().build();
-    }
-    @GetMapping("/user/kakao/callback")
-    public ResponseEntity<?> redirectKakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException, URISyntaxException {
+    //@GetMapping("/api/member/login/kakao")
+    //public ResponseEntity<Object> moveKakaoInitUrl() {
+    //    try {
+    //        URI redirectUri = new URI("https://kauth.kakao.com/oauth/authorize?client_id=" + kakaoKey + "&redirect_uri=" + kakaoRedirectUrl + "&response_type=code");
+    //        HttpHeaders httpHeaders = new HttpHeaders();
+    //        httpHeaders.setLocation(redirectUri);
+    //        return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+    //    } catch (URISyntaxException e) {
+    //        e.printStackTrace();
+    //    }
+    //    return ResponseEntity.badRequest().build();
+    //}
+    @GetMapping("/api/user/callback")
+    public ResponseEntity<?> redirectKakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         kakaoMemberService.kakaoLogin(code, kakaoKey, kakaoRedirectUrl, response);
-        return goMainPageIfSuccessful();
-    }
-
-    private ResponseEntity<Object> goMainPageIfSuccessful() throws URISyntaxException {
-        String MAIN_PAGE_URL = "http://localhost:3000";
-        URI redirectUri = new URI(MAIN_PAGE_URL);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(redirectUri);
-        return new ResponseEntity<>(httpHeaders, HttpStatus.PERMANENT_REDIRECT);
+        return ResponseEntity.ok().body(ResponseDto.success("Kakao OAuth Success"));
     }
 
 }
