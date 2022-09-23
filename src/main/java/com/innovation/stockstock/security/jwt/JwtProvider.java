1deedb9ea7d1919a4e04f041490359201528b846
lgkrwnsdll <80141718+lgkrwnsdll@users.noreply.github.com>
2022-09-23 20:48:04 +0900

    버그 수정

---
+++ src/main/java/com/innovation/stockstock/security/jwt/JwtProvider.java
@@ -0,0 +1,81 @@
+package com.innovation.stockstock.security.jwt;
+
+import com.innovation.stockstock.dto.TokenDto;
+import com.innovation.stockstock.entity.Member;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
+    private Key key;
+
+    @PostConstruct
+    protected void keyInit() {
+        key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+    }
+
+    public TokenDto generateTokenDto(Member member) {
+        long now = new Date().getTime();
+
+        int ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; // 30분
+        String accessToken = Jwts.builder()
+                .setSubject(member.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(now + ACCESS_TOKEN_EXPIRE_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        int REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14; // 2주일
+        String refreshToken = Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return new TokenDto(accessToken, refreshToken);
+    }
+
+    public boolean validateToken(String token) throws ExpiredJwtException {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+
+    public Authentication getAuthentication(String token) {
+        String email = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+}
