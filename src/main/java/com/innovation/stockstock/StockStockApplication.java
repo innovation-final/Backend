c47fc7bfb422ced07ec092c58c88ee169ef3ee8f
Meanz12 <meanz1331@gmail.com>
2022-09-27 21:41:36 +0900

    ec2 metadate 설정 변경

--- src/main/java/com/innovation/stockstock/StockStockApplication.java
+++ src/main/java/com/innovation/stockstock/StockStockApplication.java
@@ -7,10 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class StockStockApplication {
-    static {
-        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(StockStockApplication.class, args);
     }
