0c277164526f2608676713e23950ad322566279b
Meanz12 <meanz1331@gmail.com>
2022-09-28 21:28:53 +0900

    시간 설정

--- src/main/java/com/innovation/stockstock/StockStockApplication.java
+++ src/main/java/com/innovation/stockstock/StockStockApplication.java
@@ -3,10 +3,16 @@ package com.innovation.stockstock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class StockStockApplication {
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(StockStockApplication.class, args);
     }
