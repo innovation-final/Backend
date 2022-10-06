479187a5cfa22e0747aa8ba6b10a9b211afa194e
Meanz12 <meanz1331@gmail.com>
2022-10-06 15:01:59 +0900

    채팅 sockJS 적용

--- src/main/java/com/innovation/stockstock/chat/WebSocketConfig.java
+++ src/main/java/com/innovation/stockstock/chat/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp")
-                .setAllowedOriginPatterns("*");
-        //sockjs쓰면 안됨
+                .setAllowedOrigins("https://hakjoonkim.shop")
+                .withSockJS();
     }
 }
