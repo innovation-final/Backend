0116a3e72c36b8bc526630116a0dbbd6feaa340a
lgkrwnsdll <80141718+lgkrwnsdll@users.noreply.github.com>
2022-10-07 15:14:51 +0900

    프로필 변경 버그 수정

--- src/main/java/com/innovation/stockstock/dto/request/ProfileRequestDto.java
+++ src/main/java/com/innovation/stockstock/dto/request/ProfileRequestDto.java
@@ -2,11 +2,9 @@ package com.innovation.stockstock.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class ProfileRequestDto {
     private String nickname;
