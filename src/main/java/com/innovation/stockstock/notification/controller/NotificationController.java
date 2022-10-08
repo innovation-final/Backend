d6a30fb0dc4a5f01fe2a1ff12e84b3225a0bc010
Meanz12 <meanz1331@gmail.com>
2022-10-08 17:30:15 +0900

    #35 notification api change

--- src/main/java/com/innovation/stockstock/notification/controller/NotificationController.java
+++ src/main/java/com/innovation/stockstock/notification/controller/NotificationController.java
@@ -18,28 +18,28 @@ public class NotificationController {
     private final EmitterService emitterService;
     private final NotificationService notificationService;
 
-    @GetMapping("/auth/notifications")
+    @GetMapping("/api/auth/notifications")
     public ResponseEntity<?> getNotification() {
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Member member = userDetails.getMember();
         return ResponseEntity.ok().body(notificationService.getNotification(member.getId()));
     }
 
-    @GetMapping("/auth/notifications/read")
+    @GetMapping("/api/auth/notifications/read")
     public ResponseEntity<?> readOk() {
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Member member = userDetails.getMember();
         return ResponseEntity.ok().body(notificationService.readOk(member.getId()));
     }
 
-    @DeleteMapping("/auth/notifications")
+    @DeleteMapping("/api/auth/notifications")
     public ResponseEntity<?> deleteNotification() {
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Member member = userDetails.getMember();
         return ResponseEntity.ok().body(notificationService.deleteNotification(member.getId()));
     }
 
-    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    @GetMapping(value = "/api/subscribe", produces = "text/event-stream")
     public SseEmitter subscribe(@RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
         return emitterService.createEmitter(lastEventId);
     }
