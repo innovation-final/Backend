cf28af8a22f291fb2bea50a4a241393da6f132d6
Meanz12 <meanz1331@gmail.com>
2022-10-10 16:21:07 +0900

    #35 error modified

--- src/main/java/com/innovation/stockstock/notification/service/EmitterService.java
+++ src/main/java/com/innovation/stockstock/notification/service/EmitterService.java
@@ -1,34 +1,29 @@
 package com.innovation.stockstock.notification.service;
 
-import com.innovation.stockstock.member.domain.Member;
-import com.innovation.stockstock.member.repository.MemberRepository;
 import com.innovation.stockstock.notification.repository.EmitterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
 import java.util.Map;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class EmitterService {
     private final EmitterRepository emitterRepository;
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60 * 24;
-    private final MemberRepository memberRepository;
 
     public SseEmitter createEmitter(Long memberId, String lastEventId) {
-        Optional<Member> member = memberRepository.findById(memberId);
-        String emitterId = member.get().getId()+"_"+System.currentTimeMillis();
 
+        String emitterId = memberId+"_"+System.currentTimeMillis();
         SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
 
         emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
         emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
         emitter.onError(e -> {emitterRepository.deleteById(emitterId);});
 
-        String eventId = memberId + "_" + System.currentTimeMillis();
-        sendToClient(emitter, eventId, emitterId, "EventStream Created. [nickName="+member.get().getNickname()+"]");
+        String eventId = memberId+"_"+System.currentTimeMillis();
+        sendToClient(emitter, eventId, emitterId, "EventStream Created.");
 
         if (!lastEventId.isEmpty()) {
             Map<String, Object> events = emitterRepository.findAllEventCacheStartWithByMemberId(String.valueOf(memberId));
@@ -43,7 +38,6 @@ public class EmitterService {
         try {
             emitter.send(SseEmitter.event()
                     .id(eventId)
-                    .name("sse")
                     .data(data));
         } catch (IOException exception) {
             emitterRepository.deleteById(emitterId);
