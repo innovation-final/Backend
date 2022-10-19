4bef69e9ae760793e09c8f11737c6cfbe8d8d721
Meanz12 <meanz1331@gmail.com>
2022-10-19 15:10:42 +0900

    merge : branch achievement

--- src/main/java/com/innovation/stockstock/achievement/domain/MemberAchievement.java
+++ src/main/java/com/innovation/stockstock/achievement/domain/MemberAchievement.java
@@ -3,6 +3,7 @@ package com.innovation.stockstock.achievement.domain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.innovation.stockstock.member.domain.Member;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class MemberAchievement {
 
@@ -29,4 +31,9 @@ public class MemberAchievement {
     @ManyToOne
     @JoinColumn(name = "achievement_id")
     private Achievement achievement;
+
+    public MemberAchievement(Member member, Achievement achievement) {
+        this.member = member;
+        this.achievement = achievement;
+    }
 }
