5ac26446c84be70deae7c0e2c46ff7c6206cc210
lgkrwnsdll <80141718+lgkrwnsdll@users.noreply.github.com>
2022-10-19 23:02:09 +0900

    test reissue

--- src/main/java/com/innovation/stockstock/security/jwt/JwtProvider.java
+++ src/main/java/com/innovation/stockstock/security/jwt/JwtProvider.java
@@ -35,7 +35,7 @@ public class JwtProvider {
     public TokenDto generateTokenDto(Member member) {
         long now = new Date().getTime();
 
-        int ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; // 30분
+        int ACCESS_TOKEN_EXPIRE_TIME = 1000 * 30;
         String accessToken = Jwts.builder()
                 .setSubject(member.getEmail())
                 .setIssuedAt(new Date())
@@ -44,7 +44,7 @@ public class JwtProvider {
                 .compact();
 
 
-        int REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14; // 2주일
+        int REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60;
         String refreshToken = Jwts.builder()
                 .setSubject(member.getEmail())
                 .setIssuedAt(new Date())
